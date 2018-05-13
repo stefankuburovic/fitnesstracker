2171fb7c84ebaa01423ca45b2df87b49cd512915
Marko <hermes.technic@gmail.com>
2018-05-13 06:25:39 +0200

    Committtitit

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Client;
 use Illuminate\Http\Request;
+use \Illuminate\Http\JsonResponse;
 
 class ClientController extends Controller
 {
     public function index()
     {
+
+
+        $all = Client::where('trainer_id', '=', 1)->get();
+        //dd($all);
+
+        return view('trainer', compact('all'));
 
     }
 }
